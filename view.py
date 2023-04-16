db30069f2c10811578bdbebfa54ab2898c74aac7
denis <stroodenis462@mail.ru>
2023-04-16 14:09:34 +0600

    Function of displaying contacts added

--- view.py
+++ view.py
@@ -8,3 +8,13 @@ def main_menu() -> int:
         choice = input(txt.choice_menu)
         if choice.isdigit() and 0 < int(choice) < 9:
             return int(choice)
+
+
+def show_contacts(book: list[Contact], message: str) -> None:
+    print('\n' + '-' * 70)
+    if book:
+        for i, contact in enumerate(book, 1):
+            print(f'{i}. {contact}')
+    else:
+        print(message)
+    print('-' * 70 + '\n')
